05fc4f54671d54bab0a762c2ae3c84b33b7c3cda
mamepuro <beans.mamegame.laugh@icloud.com>
2019-09-03 23:50:44 +0900

    add

---
+++ magical-lithography/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace magicallithography
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            asd.Engine.Initialize("magical-lithography",1080,800,new asd.EngineOption());
+            
+        }
+    }
+}
---
+++ magical-lithography/TitleScene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace magicallithography
+{
+    public class TitleScene
+    {
+        public TitleScene()//constructor
+        {}
+        protected override void OnREgister
+    }
+}
