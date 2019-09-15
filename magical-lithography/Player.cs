615b849a415402a9f301e19b87cb70be9c399897
mamepuro <beans.mamegame.laugh@icloud.com>
2019-09-15 20:00:28 +0900

    add

--- magical-lithography/Player.cs
+++ magical-lithography/Player.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 namespace magicallithography
 {
     public class Player:asd.TextureObject2D
-    { 
-        const float gravity = 0.98f;
-        bool isFlying = false;
-        bool isLanding = true;
-        int jumpcount;
-        const float jumpfirstvelocity = -100.0f;
-        float jumpvelocity;
+    {
+        //const float gravity = 12.0f;
+        //float time =1.0f;
+        //float movedeistance1;
+        //float movedeistance2 = 0.0f;
+        bool isFlying = false;//ジャンプ中かのフラグ
+        //bool isLanding = true;
+        float jumpcount = 1.0f;
+        const float jumpfirstvelocity = -100.0f;//ジャンプの初速
+        float jumpvelocity1;//移動距離
+        float jumpvelocity2 = 0.0f;
+
         //private float jumppos = 0.0f;
         public Player()//constructor
         {
@@ -30,32 +35,24 @@ namespace magicallithography
             {
                 this.Position += new asd.Vector2DF(-3.0f, 0.0f);
             }
-            if(asd.Engine.Keyboard.GetKeyState(asd.Keys.Up) == asd.KeyState.Push)
+            if(asd.Engine.Keyboard.GetKeyState(asd.Keys.Up) == asd.KeyState.Push )
             {
                 isFlying = true;
-                isLanding = false;
-                jumpcount = 30;
-                jumpvelocity = jumpfirstvelocity;
+                //isLanding = false;
+                //jumpcount = 30;
+                //jumpvelocity = jumpfirstvelocity;
             }
-            if(isFlying == true)
+            if (isFlying && asd.Engine.Keyboard.GetKeyState(asd.Keys.Up) == asd.KeyState.Hold)
             {
-                if (jumpvelocity < 0)
+                jumpvelocity1 = 5.0f * (jumpcount - 10.0f) * (jumpcount - 10.0f) - 500.0f;
+                Position += new asd.Vector2DF(0.0f, (jumpvelocity1 - jumpvelocity2) * -1.0f);
+                jumpcount += 0.1f;
+                jumpvelocity2 = jumpvelocity1;
+                if(jumpcount != 0.0f && jumpvelocity2 == 0)
                 {
-                    this.Position += new asd.Vector2DF(0.0f, jumpvelocity);
-                    jumpvelocity += 20.0f;
-                }
-                else
-                {
-                    jumpvelocity += 20.0f;
-                    this.Position += new asd.Vector2DF(0.0f, jumpvelocity);
-                    if(jumpvelocity >= 100.0f)
-                    {
-                        isFlying = false;
-                    }
+                    jumpcount = 1.0f;
                 }
             }
-
-            
             //画面内に収まるようにする
             asd.Vector2DF position = this.Position;
             position.X = asd.MathHelper.Clamp(position.X, asd.Engine.WindowSize.X - Texture.Size.X , 0.0f);//asd.MathHelper.Clamp(制御したい変数,最大値,最小値)
